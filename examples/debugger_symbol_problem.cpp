ac63f400a8a2925167409b22fa0d5d69ca1c17da
iyume <iyumelive@gmail.com>
2023-11-25 13:34:55 +0800

    add comment

--- examples/debugger_symbol_problem.cpp
+++ examples/debugger_symbol_problem.cpp
@@ -4,4 +4,5 @@ void A::func() {
     aa.insert({"1", "1"});
     aa.insert({"2", "2"});
     aa.size();  // make a symbol for debugger to call
+    // make breakpoint here and print aa will show empty aa
 }
--- examples/debugger_symbol_problem_main.cpp
+++ examples/debugger_symbol_problem_main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::unordered_map<std::string, std::string> aa{{"1", "1"}};
+    // without above line, a problem occurs in debugger_symbol_problem.cpp
     auto a = A{};
     a.func();
 }
